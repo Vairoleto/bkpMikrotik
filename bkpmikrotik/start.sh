9418e45f6314adc3142c88cde6260c613644aa58
Vairoleto <ggabas@outlook.com>
2018-05-16 17:06:46 -0300

    Update start.sh

--- bkpmikrotik/start.sh
+++ bkpmikrotik/start.sh
@@ -6,4 +6,4 @@ crond
 
 # generate host keys if not present
 # do not detach (-D), log to stderr (-e), passthrough other arguments
-[ ! -f /bkpmikrotik/keys/id_rsa ] && ssh-keygen -b 2048 -t rsa -f /bkpmikrotik/keys/id_rsa -q -N "" || exec /usr/sbin/sshd -D -e -h /bkpmikrotik/keys/id_rsa "$@"
+[ ! -f /bkpmikrotik/keys/id_rsa ] && ssh-keygen -b 2048 -t rsa -f /bkpmikrotik/keys/id_rsa -q -N "" && exec /usr/sbin/sshd -D -e -h /bkpmikrotik/keys/id_rsa "$@" || exec /usr/sbin/sshd -D -e -h /bkpmikrotik/keys/id_rsa "$@"
